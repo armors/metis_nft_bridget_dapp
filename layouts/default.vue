7df548f745e67634fbbf55cd0d9d244178e99d14
EternalShallow <eternalShallow@163.com>
2022-03-17 20:04:18 +0800

    word

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,8 +31,9 @@
 <!--            <a href="javascript:;" class="history-text">View your history ></a>-->
 <!--          </div>-->
           <div class="display-flex box-center tag">
-            <div>Terms of Service</div>
-            <div class="policy">Privacy policy</div>
+            <a target="_blank" href="https://docs.google.com/document/d/1rxvw45mTeinA2T_dFhC0R42-B1S0oJY0/edit?usp=sharing&ouid=100792652384418651726&rtpof=true&sd=true ">Terms of Service</a>
+            <a target="_blank" href="https://docs.google.com/document/d/1eCwsWWY3ZbD9ls4iOLqblOG_H0ekxXZ0/edit?usp=sharing&ouid=100792652384418651726&rtpof=true&sd=true" class="policy">Privacy policy</a>
+            <a target="_blank" href="https://docs.metis.io/metis-nft-bridge" class="policy">User Guide</a>
           </div>
           <div>© Copyright 2021 by MetisLab Foundation Ltd.</div>
         </div>
@@ -605,6 +606,9 @@ export { default } from './js/default'
       }
       .tag{
         padding-top: 64px;
+      }
+      a{
+        color: rgba(255, 255, 255, 0.5);
       }
       /*.history-text{*/
       /*  margin: 0 auto;*/

6ff7b585bb1778bfe43734af0fe91a72a7300d38
EternalShallow <eternalShallow@163.com>
2022-03-19 21:41:41 +0800

    更新主网bridge bridgefactory合约地址

--- store/network.js
+++ store/network.js
@@ -57,8 +57,8 @@ export const networkEthereum = {
   },
   rpcUrls: ['https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'],
   blockExplorerUrls: ['https://etherscan.io/'],
-  bridgeFactory: '0x4076654b188345A61Ad93143F7E64c4427B4531f',
-  bridge: '0x5126F650ca04bc6cB0C47ea2654e997ae5670120',
+  bridgeFactory: '0xc6c9efe77E8De46452DDfFcE8718a8186c661380',
+  bridge: '0x5EA23Cb3D609F4522a21ADcC9Ca366e76C23c40f',
   oracleContract: '0xa9BdA5A0881e59587B119685027a601Cae1Cc83d',
   oracleAbi: 'iMVM_DiscountOracle',
   domainInfo: {
@@ -81,8 +81,8 @@ export const networkAndromeda = {
   },
   rpcUrls: ['https://andromeda.metis.io/?owner=1088'],
   blockExplorerUrls: ['https://andromeda-explorer.metis.io/'],
-  bridgeFactory: '0x631c6A5f50666f68F4C1A1E8e4c868b04935dD87',
-  bridge: '0x831844F04c26241a62eCb074Cc27075bA1686DE9',
+  bridgeFactory: '0xDb3439640a0C8273E9c81bB2D9D4fa34fa180e17',
+  bridge: '0xB64585Bd0d686EaeD47eE986190eA8abE610ae33',
   oracleContract: '0x420000000000000000000000000000000000000F',
   oracleAbi: 'iOVM_GasPriceOracle',
   domainInfo: {
